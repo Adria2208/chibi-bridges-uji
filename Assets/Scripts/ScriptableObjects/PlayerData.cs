823ad41cfb28fad3845b4d951f4bd9bec038e148
Adrià Sánchez López <57090378+Adria2208@users.noreply.github.com>
2026-02-18 15:46:04 +0100

    Refactor PlayerData

--- Assets/Scripts/ScriptableObjects/PlayerData.cs
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
 public class PlayerData : ScriptableObject
 {
+    [Header("Horizontal movement parameters")]
     public float maxSpeed;
     public float acceleration;
     public float deceleration;
+    
+    [Header("Jump parameters")]
     public float jumpForce;
+    [Range(0f, 0.5f)]
     public float jumpBufferTime;
+    [Range(0f, 1f)]
     public float variableJumpTime;
     public float fastFallMultiplier;
     public float originalGravityScale;
